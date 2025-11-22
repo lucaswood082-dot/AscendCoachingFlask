cc80ece29a0e24a73b8d3f8f1a7d839fd1d3a912
Lucas <lucaswood@Lucas-Laptop.modem>
2025-11-22 17:06:17 +1030

    Reset PWA setup: base.html, sw.js, manifest, icons

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,6 @@
 const CACHE_NAME = 'ascend-cache-v1';
 const urlsToCache = [
   '/',
-  '/static/style.css',
-  '/static/sw.js',
-  '/static/manifest.json',
-  '/static/icons/icon-192x192.png',
-  '/static/icons/icon-512x512.png',
   '/about',
   '/programs',
   '/nutrition',
@@ -14,35 +9,29 @@ const urlsToCache = [
   '/contact',
   '/optimal',
   '/static/style.css',
-  '/static/sw.js',
-  '/static/manifest.json',
   '/static/icons/icon-192x192.png',
-  '/static/icons/icon-512x512.png'
+  '/static/icons/icon-512x512.png',
+  '/static/manifest.json'
 ];
 
-  // Add more pages or images here
-
-// Install event: cache files
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event: clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => {
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
         if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
+      })
+    ))
   );
 });
 
-// Fetch event: respond with cached resources or fetch from network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
